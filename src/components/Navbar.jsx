ace7b62e139e39b12f5154054a3e022d6e54bd28
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-02-11 00:43:33 +0400

    color changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       >
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            <img src="../src/images/modeluxe.png" alt="" />
+            <img src="../src/images/logo-modo.png" alt="" />
           </a>
           <button
             className="navbar-toggler collapsed"
