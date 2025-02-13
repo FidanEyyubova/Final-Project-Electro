4b80a349f53c72c195a5d7309a1728fa366167de
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-02-13 21:33:52 +0400

    home toppixk

--- src/pages/TopPicks.jsx
+++ src/pages/TopPicks.jsx
@@ -1,53 +1,82 @@
 import React, { useContext } from "react";
 import { MyContext } from "../context/MyProvider";
+import { Link } from "react-router-dom";
 
 const TopPicks = () => {
   const { product, setProduct } = useContext(MyContext);
   const handleColor = (id, color) => {
-    setData((prev) =>
-      prev.map((car) => {
-        if (car.id === id) {
-          const newCheckImg = Object.keys(car.checkImg).reduce((acc, key) => {
+    setProduct((prev) =>
+      prev.map((furn) => {
+        if (furn.id === id) {
+          const newCheckImg = Object.keys(furn.checkImg).reduce((acc, key) => {
             acc[key] = false;
             return acc;
           }, {});
 
           newCheckImg[color] = true;
 
-          return { ...car, checkImg: newCheckImg };
+          return { ...furn, checkImg: newCheckImg };
         }
-        return car;
+        return furn;
       })
     );
   };
 
   return (
-    <div className="row">
-      {product.slice(0,6).map((el) => (
-        <div key={el.id} className="col-lg-3 col-md-4 col-6">
-          <div>
-            {Object.keys(el.checkImg).map((color) =>
-              el.checkImg[color] ? <img key={color} src={el.linkImg[color]} alt={color} /> : null
-            )}
-          </div>
-          <div className="color">
-            {el.colors?.map((color) => (
-              <p
-                key={color}
-                style={{
-                  backgroundColor: color,
-                  width: "100px",
-                  height: "100px",
-                  borderRadius: "50%",
-                  cursor: "pointer",
-                  border: el.checkImg[color] ? "3px solid black" : "none",
-                }}
-                onClick={() => handleColor(el.id, color)}
-              ></p>
-            ))}
+    <div className="top">
+      <div className="container-fluid">
+        <div className="row text-center">
+          <div className="col-12">
+            <h1>Top Picks</h1>
+            <p>
+              Find a bright ideal to suit your taste with our great selection of
+              suspension, floor and table lights.
+            </p>
           </div>
         </div>
-      ))}
+        <div></div>
+        <div className="row mx-3">
+
+          {product.slice(0, 8).map((el) => (
+            <div key={el.id} className="col-lg-3 col-md-4 col-6">
+              <div className="image  text-center">
+                {Object.keys(el.checkImg).map((color) =>
+                  el.checkImg[color] ? (
+                    <img key={color} src={el.linkImg[color]} alt={color} />
+                  ) : null
+                )}
+              </div>
+              <div>
+                <div>
+                    <p>{el.title}</p>
+                </div>
+              <div className="color d-flex gap-2">
+                {el.colors?.map((color) => (
+                  <p
+                    key={color}
+                    style={{
+                      backgroundColor: color,
+                      width: "30px",
+                      height: "30px",
+                      borderRadius: "50%",
+                      cursor: "pointer",
+                      border: el.checkImg[color] ? "1px solid black" : "none",
+                    }}
+                    onClick={() => handleColor(el.id, color)}
+                  ></p>
+                ))}
+              </div>
+              <div>
+                    <p>${el.price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="text-center py-5">
+        <Link className="view px-5 py-3">View More</Link>
+        </div>
+      </div>
     </div>
   );
 };
