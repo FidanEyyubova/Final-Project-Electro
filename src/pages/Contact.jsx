0098cc3a4da9e31def455f835ca793a344670783
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-02-27 14:54:28 +0400

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             </h1>
           </div>
         </div>
-        <div className="row vision d-flex justify-content-center align-items-center py-5">
+        <div className="row vision vision-b d-flex justify-content-center align-items-center py-5">
           <div
             className="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center text-center"
             data-aos="fade-down"
@@ -68,44 +68,72 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-12 g-0">
-            <div>
-              <img
-                src="https://mb-demo1.myshopify.com/cdn/shop/files/contact-banner.jpg?v=1687412566&width=1920"
-                alt=""
-              />
+        <div className="row conc d-flex justify-content-center align-items-center py-4">
+          <div className="col-12 d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-column justify-content-center align-items-center py-5">
+              <h1 className="mb-4">Keep in touch with us</h1>
+              <div className="row d-flex justify-content-center align-items-center">
+                <div className="col-lg-6  d-flex justify-content-center align-items-center">
+                  <div className="d-flex flex-column  d-flex justify-content-center align-items-center">
+                    <div className="d-flex flex-column gap-4 ">
+                      <input type="text" placeholder="Your full name" />
+                      <input type="email" placeholder="Your email address" />
+                      <input type="tel" placeholder="Your mobile number" />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="mt-1 pt-1">
+                    <textarea
+                      name=""
+                      id=""
+                      placeholder="Your message  here..."
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="d-flex justify-content-end align-items-center">
+                    <div className="form-check d-flex my-3 gap-1">
+                      <input
+                        type="checkbox"
+                        className="check mt-2"
+                        id="termsCheck"
+                      />
+                      <label
+                        className="form-check-label ms-2"
+                        htmlFor="termsCheck"
+                      >
+                        I accept the terms & conditions and I understand that my
+                        data will be held securely in accordance with the
+                        privacy policy.
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-lg-6">
+                  <div className="my-3">
+                    <button className="btn btn-danger">Send Message</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="row conc d-flex justify-content-center align-items-center">
-          <div className="col-12 d-flex justify-content-center align-items-center">
-            <div className="d-flex flex-column justify-content-center align-items-center">
-              <h3>Keep in touch with us</h3>
-              <div className="d-flex">
-                <div>
-                  <form action="" className="d-flex flex-column">
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
-                  </form>
-                </div>
-                <div>
-                  <form action="" className="d-flex flex-column">
-                    <textarea name="" id=""></textarea>
-                    
-                  </form>
-                </div>
-              </div>
-              <div>
-                <input type="checkbox" />
-                <span>
-                  I accept the terms & conditions and I understand that my data
-                  will be <br /> hold securely in accordance with the privacy
-                  policy.
-                </span>
-                <button className="btn btn-danger">Send </button>
-              </div>
+        <div className="row my-4">
+          <div className="col-12 p-0">
+            <div
+              className="map-container"
+              style={{ width: "100%", maxWidth: "1700px" }}
+            >
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12880.636965452359!2d-115.07830475045652!3d36.18700888383667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c8dc928cfe64e7%3A0xa7db8399b4bb0217!2sN%20Ringe%20Ln%2C%20Sunrise%20Manor%2C%20NV%2C%20USA!5e0!3m2!1sen!2saz!4v1740653097852!5m2!1sen!2saz"
+                style={{ border: 0, width: "100%", height: "400px" }}
+                loading="lazy"
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         </div>
